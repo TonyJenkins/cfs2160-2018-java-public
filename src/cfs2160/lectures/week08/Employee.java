4c94a27161ef9a8c6aeb2b53a8b593058b808e37
Tony Jenkins <tony.jenkins@elder-studios.co.uk>
2018-11-28 15:42:06 +0000

    Add stubs of Week 8 lecture programs

---
+++ src/cfs2160/lectures/week08/Employee.java
@@ -0,0 +1,12 @@
+package cfs2160.lectures.week08;
+
+public class Employee {
+
+    private int id;
+    private String name;
+    private int rating;
+
+    public Employee () {
+    }
+
+}
---
+++ src/cfs2160/lectures/week08/EmployeeDemo.java
@@ -0,0 +1,9 @@
+package cfs2160.lectures.week08;
+
+public class EmployeeDemo {
+
+    public static void main (String[] args) {
+
+        Employee e = new Employee ();
+    }
+}
