0e64c7b4a9a3c37db69390da84a8f06f6f42ea3b
Tony Jenkins <tony.jenkins@elder-studios.co.uk>
2018-11-29 15:04:58 +0000

    Include final code from Week 9 lecture

--- src/cfs2160/lectures/week08/Employee.java
+++ src/cfs2160/lectures/week08/Employee.java
@@ -9,4 +9,63 @@ public class Employee {
     public Employee () {
     }
 
+    public Employee (int id, String name, int rating) {
+        this.id = id;
+        this.name = name;
+        this.rating = rating;
+    }
+
+    public int getId () {
+        return id;
+    }
+
+    public void setId (int id) {
+        this.id = id;
+    }
+
+    public String getName () {
+        return name;
+    }
+
+    public void setName (String name) {
+        this.name = name;
+    }
+
+    public int getRating () {
+        return rating;
+    }
+
+    public void setRating (int rating) {
+        this.rating = rating;
+    }
+
+    public boolean upRating () {
+        if (this.rating == 10) {
+            return false;
+        }
+        else {
+            this.rating++;
+            return true;
+        }
+    }
+
+    public boolean downRating () {
+        if (this.rating == 0) {
+            return false;
+        }
+        else {
+            this.rating--;
+            return true;
+        }
+    }
+
+    @Override
+    public String toString () {
+        final StringBuilder sb = new StringBuilder ("Employee{");
+        sb.append ("id=").append (id);
+        sb.append (", name='").append (name).append ('\'');
+        sb.append (", rating=").append (rating);
+        sb.append ('}');
+        return sb.toString ();
+    }
 }
--- src/cfs2160/lectures/week08/EmployeeDemo.java
+++ src/cfs2160/lectures/week08/EmployeeDemo.java
@@ -4,6 +4,22 @@ public class EmployeeDemo {
 
     public static void main (String[] args) {
 
-        Employee e = new Employee ();
+        Employee e = new Employee (123, "John Smith", 10);
+
+        System.out.println ("e = " + e);
+
+        System.out.println ("Checking rating cannot be > 10 ...");
+
+        if (!e.upRating ()) {
+            System.out.println ("It doesn't work. Cool Beans.");
+        }
+        else {
+            System.out.println ("It worked. How sad.");
+        }
+
+        System.out.println ("e = " + e);
+
+
+        
     }
 }
